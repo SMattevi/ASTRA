ea3b4b54566d00f6017b0625508c10d95133bf03
SMattevi <s.mattevi@unibs.it>
2023-07-18 14:09:56 +0000

    app creation

--- my_app/app.R
+++ my_app/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ASTRA)
+library(ggplot2)
 
 # Define UI for data upload app ----
 ui <- fluidPage(
@@ -14,83 +15,58 @@ ui <- fluidPage(
     sidebarPanel(
 
       # Input: Select a file ----
-      fileInput("vcf", "Input het SNPs vcf (gzipped)",
+      fileInput("rds", "Input ASTRA result",
                 multiple = FALSE),
-      fileInput("shapeit", "Input het SNPs vcf (gzipped) phased with SHAPEIT4",
-                multiple = FALSE),
-      fileInput("haptree", "Input het SNPs vcf (gzipped) phased with HapTree-X",
-                multiple = FALSE),
-
-      # Horizontal line ----
-      tags$hr(),
-
-      # Input: Select number of rows to display ----
-      radioButtons("disp", "Display",
-                   choices = c(Head = "head",
-                               All = "all"),
-                   selected = "head"),
 
       # Horizontal line ----
       tags$hr(),
 
       #Input: Select chromosome number
-      textInput("chrom", label = h3("Chromosome number"), value = "1"),
+      textInput("genes", label = h3("Gene(s) to plot (separate by \",\")"), value = "MEG3,BAG5"),
 
       # Horizontal line ----
       tags$hr(),
 
       #Input: Select chromosome number
-      textInput("sampleid", label = h3("Sample name"), value = "SampleID"),
+      textInput("title", label = h3("Plot title"), value = "ASTRA result"),
+
+      actionButton("do", "Click Me")
     ),
 
     # Main panel for displaying outputs ----
     mainPanel(
-
-      # Output: Data file ----
-      tableOutput("contents")
-
+      plotOutput("plot")
     )
-
   )
 )
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
+  df_input<-
+  observeEvent(input$do, {
+    output$plot <- renderPlot({
 
-  output$contents <- renderTable({
+      # input$file1 will be NULL initially. After the user selects
+      # and uploads a file, head of that data file by default,
+      # or all rows if selected, will be shown.
 
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
+      req(input$rds)
 
-    req(input$vcf)
-    req(input$shapeit)
-    req(input$haptree)
-    df1 <- read_shapeit(input$vcf$datapath, input$shapeit$datapath,
-                       sample_name = input$sampleid)
-    df2 <- read_haptreex(input$vcf$datapath, input$haptree$datapath,
-                       sample_name = input$sampleid)
-    BM<-biomart_df()
-    BMsel<-BM[BM$chromosome_name%in%input$chrom,]
-    # when reading semicolon separated files,
-    # having a comma separator causes `read.csv` to error
-    tryCatch(
-      {
-        df <- conshap(df1[df1$CHROM==input$chrom,],df2[df2$CHROM==input$chrom,],BMsel,CHROM = input$chrom)
-      },
-      error = function(e) {
-        # return a safeError if a parsing error occurs
-        stop(safeError(e))
+      df_input<-readRDS(input$rds$datapath)
+
+
+      isolate(p<-plot_ase(df=df_input,genes_to_plot=as.list(strsplit(input$genes, ","))[[1]],input$title))
+      if(!is.null(p)){
+        plot(p)
+      }else{
+        p<-ggplot() +
+          annotate("text", x = 4, y = 25, size=8, label = "No SNPs in the selected gene(s)") +
+          theme_void()
+        plot(p)
       }
-    )
-
-    if(input$disp == "head") {
-      return(head(df))
-    }
-    else {
-      return(df)
-    }
-
+      # when reading semicolon separated files,
+      # having a comma separator causes `read.csv` to error
+    })
   })
 
 }
