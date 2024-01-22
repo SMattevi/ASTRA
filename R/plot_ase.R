246441bab18c9a3f3a55a6257d074db825b76798
SMattevi <s.mattevi@unibs.it>
2024-01-22 13:32:02 +0000

    plot annotation

--- R/plot_ase.R
+++ R/plot_ase.R
@@ -43,3 +43,75 @@ plot_ase<- function(df,genes_to_plot,title){
     return(plot_res)
   }
 }
+
+#' Plot functional annotation of ASE/ASA analysis results. Plotting function from https://r-graph-gallery.com/297-circular-barplot-with-groups.html
+#'
+#' @param df A dataframe.
+#' @param ASC Allele Specific categorization in "biallelic/monoallelic/other" column identifier.
+#' @param gene Gene-id column identifier.
+#' @param annotation Functional annotation column to plot
+#'
+#' @import fmsb
+#' @importFrom reshape2 dcast
+#'
+#' @return A plot
+#' @export
+#'
+plot_annotation<- function(df,ASC,gene,annotation){
+  data<-aggregate(df[[gene]] ~ as.factor(df[[ASC]]) + df[[annotation]], FUN = length)
+  colnames(data)<-c("ASC","annotation","gene")
+  #data = data %>% arrange(ASC, gene)
+  data=reshape2::dcast(data,ASC~annotation,value.var = "gene")
+  data[is.na(data)]=0
+  rownames(data)<-data$ASC
+  data<-data[,-1]
+
+  data <- rbind(rep(max(data),ncol(data)) , rep(0,ncol(data)) , data)
+
+  # Color vector
+  colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
+  colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+
+  # plot with default options:
+  fmsb::radarchart( data  , axistype= 1,
+              #custom polygon
+              pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+              #custom the grid
+              cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0,max(data),max(data)/4), cglwd=0.8,
+              #custom labels
+              vlcex=0.8
+  )
+
+  # Add a legend
+  legend(x=0.7, y=1, legend = rownames(data[-c(1,2),]), bty = "n", pch=20 , col=colors_in , text.col = "grey", cex=1.2, pt.cex=3)
+  # # Set a number of 'empty bar'
+  # empty_bar <- 3
+  #
+  # # Add lines to the initial dataset
+  # to_add <- matrix(NA, empty_bar, ncol(data))
+  # colnames(to_add) <- colnames(data)
+  # data <- rbind(data, to_add)
+  # data$id <- seq(1, nrow(data))
+  #
+  # # Get the name and the y position of each label
+  # label_data <- data
+  # number_of_bar <- nrow(label_data)
+  # angle <- 90 - 360 * (label_data$id-0.5) /number_of_bar     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
+  # label_data$hjust <- ifelse( angle < -90, 1, 0)
+  # label_data$angle <- ifelse(angle < -90, angle+180, angle)
+  #
+  # # Make the plot
+  # p <- ggplot(data, aes(x=as.factor(id), y=gene)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
+  #   geom_bar(stat="identity", aes(fill=ASC)) +
+  #   ylim(-100,max(data$gene[!is.na(data$gene)])+20) +
+  #   theme_minimal() +
+  #   theme(
+  #     axis.text = element_blank(),
+  #     axis.title = element_blank(),
+  #     panel.grid = element_blank(),
+  #     plot.margin = unit(rep(-1,4), "cm")
+  #   ) +
+  #   coord_polar(start = 0) +
+  #   geom_text(data=label_data, aes(x=id, y=gene+10, label=annotation, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE )
+  # return(p)
+}
